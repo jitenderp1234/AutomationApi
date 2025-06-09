55443a8496ba141b866f051342f37d26a6e7582a
jitenderp1234 <60725676+jitenderp1234@users.noreply.github.com>
2025-06-09 17:13:40 +0530

    Add files via upload

---
+++ AutomationApi/yaml/install-front-door-end-to-end-tls.sh
@@ -0,0 +1,82 @@
+# Install kubectl
+az aks install-cli --only-show-errors
+
+# Get AKS credentials
+az aks get-credentials \
+  --admin \
+  --name $clusterName \
+  --resource-group $resourceGroupName \
+  --subscription $subscriptionId \
+  --only-show-errors
+
+# Check if the cluster is private or not
+private=$(az aks show --name $clusterName \
+  --resource-group $resourceGroupName \
+  --subscription $subscriptionId \
+  --query apiServerAccessProfile.enablePrivateCluster \
+  --output tsv)
+  
+# Create values.yaml file for Istio
+echo "Creating values.yaml file for Istio..."
+cat <<EOF >valuesIstio.yaml
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+spec:
+  profile: default 
+  components:
+    ingressGateways:
+      - name: istio-ingressgateway
+        enabled: true
+        k8s:
+          serviceAnnotations:
+            service.beta.kubernetes.io/azure-load-balancer-internal: "true"
+EOF
+  
+# Install Istio
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.26.0 sh -
+cd istio-1.26.0
+istioctl install --set profile=default -f valuesIstio.yaml
+cd -
+
+# Install Helm
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 -o get_helm.sh -s
+chmod 700 get_helm.sh
+./get_helm.sh &>/dev/null
+
+# Add Helm repos
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add kiali https://kiali.org/helm-charts
+helm repo add jetstack https://charts.jetstack.io
+
+# Update Helm repos
+helm repo update
+
+# Install Kiali
+#if [[ "$installKiali" == "true" ]]; then
+  echo "Installing Kiali..."
+  helm install \
+    --set cr.create=true \
+    --set cr.namespace=istio-system \
+    --set cr.spec.auth.strategy="anonymous" \
+    --namespace kiali-operator \
+    --create-namespace \
+    kiali-operator \
+    kiali/kiali-operator
+#fi
+
+# Install Prometheus
+if [[ "$installPrometheusAndGrafana" == "true" ]]; then
+  echo "Installing Prometheus and Grafana..."
+  helm install prometheus prometheus-community/kube-prometheus-stack \
+    --create-namespace \
+    --namespace prometheus \
+    --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false \
+    --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false
+fi
+
+
+
+# Create an ingress resource for the application
+echo "Label namesspace"
+kubectl label namespace default istio-injection=enabled
+
