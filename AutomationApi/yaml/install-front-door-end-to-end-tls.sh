db25357018bf6c1b4a19cc9e0e65511163e675bb
jitenderp1234 <60725676+jitenderp1234@users.noreply.github.com>
2025-06-17 15:17:39 +0530

    Update install-front-door-end-to-end-tls.sh

--- AutomationApi/yaml/install-front-door-end-to-end-tls.sh
+++ AutomationApi/yaml/install-front-door-end-to-end-tls.sh
@@ -73,18 +73,10 @@ helm repo update
 #fi
 
 # Install Prometheus
-# Install Prometheus
 if [[ "$installPrometheusAndGrafana" == "true" ]]; then
-  echo "Installing Prometheus and Grafana..."
-  helm install prometheus prometheus-community/kube-prometheus-stack \
-    --create-namespace \
-    --namespace istio-system \
-	--set grafana.enabled=true \
-    --set grafana.defaultDashboardsEnabled=true \
-    --set prometheus.prometheusSpec.podMonitorSelectorNilUsesHelmValues=false \
-    --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false
+  echo "Installing Prometheus..."
+  kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/prometheus.yaml
 fi
-
 
 
 # Create an ingress resource for the application
